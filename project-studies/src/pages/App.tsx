dd6b324dfaf27c4488b02d0e423672e8e0d04d52
Daniel Thielmann <thielmanndan@gmail.com>
2024-01-08 21:30:55 -0300

    cronometro identificando as tarefas

--- project-studies/src/pages/App.tsx
+++ project-studies/src/pages/App.tsx
@@ -20,7 +20,7 @@ function App() {
       tarefas={tarefas} 
       selecionaTarefa={selecionaTarefa}
       />
-      <Cronometro />
+      <Cronometro selecionado={selecionado}/>
     </div>
   );
 }
